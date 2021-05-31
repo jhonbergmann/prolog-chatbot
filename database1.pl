1a94edf8528e2bf6e3b6129aa1fb334650cb0db5
Jhonatan Bergmann <54135650+JhonatanBergmann@users.noreply.github.com>
2021-05-31 20:57:52 -0300

    🎇 database

---
+++ database1.pl
@@ -0,0 +1,97 @@
+% data_base
+
+definida_por('rede de computadores',
+	'De uma maneira simplificada, uma rede de computadores pode ser definida 
+como uma estrutura de computadores e dispositivos conectados atraves de um 
+sistema de comunicacao com o objetivo de compartilharem informacoes e recursos entre si. 
+Tal sistema envolve meios de transmissao e protocolos.').
+
+definida_por('classificação de redes',
+	'As principais sao: Rede Local - LAN, Rede Metropolitana - MAN, Rede de Longa Distancia - WAN, Rede Local Sem Fio - WLAN...').
+
+definida_por('roteador',
+	'Um roteador e um dispositivo que encaminha pacotes de dados entre redes de computadores, 
+criando um conjunto de redes de sobreposicao.').
+
+definida_por('placa de rede',
+	'Uma placa de rede e um dispositivo de hardware responsavel pela comunicacao de um computador em uma rede de computadores.').
+	 
+definida_por('modem',
+	'Um modem e um dispositivo de hardware que converte dados em um formato adequado 
+para um meio de transmissao de forma que possam ser transmitidos de um computador para outro.').
+	 
+definida_por('repetidor',
+	'Em informatica, repetidor e um equipamento eletronico utilizado para a interligacao de redes 
+identicas, pois eles absorvem eletricamente os sinais e os retransmite pelo mesmo segmento no meio fisico.').
+	 
+definida_por('switch',
+	'È um elemento utilizado para interligar computadores e redes a fim de concentrar o
+cabeamento e permitir a conexão de redes de tipos diferentes.').
+	 
+definida_por('rede LAN',
+	'Uma rede de area local em computação consiste de uma rede de computadores 
+utilizada na interconexao de equipamentos processadores, cuja finalidade e a troca de dados.').
+	 
+definida_por('rede MAN',
+	'MAN significa Metropolitan Area Network, ou seja, rede de area metropolitana. 
+Sao projetadas justamente para interconectar sistemas de cidades proximas ou de uma regiao metropolitana.').
+	 
+definida_por('rede WAN',
+	'Uma rede de longa distancia ou rede de area alargada e uma rede de computadores que abrange uma grande area geografica, 
+com frequencia um pais ou continente.').
+	 
+definida_por('internet',
+	'A internet e uma rede mundial que tem como objetivo interligar computadores para fornecer ao usuario o acesso a diversas informacoes. 
+Por isso e chamada de rede mundial de computadores.').
+
+definida_por('correio eletronico',
+	'Um dos primeiros servicos disponiveis para a comunicacao em rede. O correio eletronico
+permite que usuarios, enviem e recebam mensagens eletronicas, usando um endereco eletronico
+como referencia para a localizacao dos destinatarios da mensagem.').
+
+definida_por('cabo coaxial',
+	'O cabo coaxial e um tipo de cabo usado para transmitir sinais. Este tipo de cabo e constituido por um fio de cobre 
+condutor revestido por um material isolante e rodeado de uma blindagem.').
+
+definida_por('cabo de par trancado',
+	'Cabo com pares de fios trancado e o mais usado em redes. Alem da flexibilidade,
+garante uma velocidade na troca das informacoes compativel com as interfaces de redes.
+Geralmente, e utilizado em redes locais.').
+
+definida_por('fibra optica',
+	'Fibra optica e um filamento flexivel e transparente fabricado a partir de vidro ou plastico extrudido e que e utilizado 
+como condutor de elevado rendimento de luz, imagens ou impulsos codificados. Tem diametro de alguns micrometros, ligeiramente 
+superior ao de um fio de cabelo humano.').
+
+definida_por('radiofrequencia',
+	'E um meio não fisico utilizado para transmitir informacoes. A este tipo de meio
+convencionou-se chamar de wireless e as tecnologias mais conhecidas que desse se utilizam sao o
+telefone celular, wi-fi, wimax, satélite, infravermelho e bluetooth.').
+
+definida_por('topologias de redes',
+	'Forma como as interfaces sao conectadas umas as outras. Essa caracteristica define o
+seu tipo, a eficiencia da rede e sua velocidade.').
+
+definida_por('topologia parcialmente ligada',
+	'Sao computadores interligados de forma intermediaria, ou seja, nao sao todos os computadores 
+que sao interligados.').
+
+definida_por('topologia em barramento',
+	'Caracteriza por ter um meio de comunicacao comum, ou seja, todos os computadores sao conectados a esse
+meio para estabelecer comunicacao entre si.').
+
+definida_por('topologia ponto a ponto',
+	'Caracteriza por enviar os dados apenas em um sentido como, por exemplo, sentido anti-horario.').
+
+definida_por('topologia multiponto',
+	'Este tipo de topologia e tambem conhecido como token ring. Caracteriza-se por circular
+no anel, um conjunto padrao de oito bits, chamado de token.').
+
+definida_por('topologia em estrela',
+	'Caracteriza-se por ter um ponto central para interligar os computadores a fim de
+estabelecer comunicacoes. O ponto central age como centro de controle da rede, interligando
+todas as maquinas.').
+
+definida_por('exit',
+	'Te espero novamente para solucionar suas duvidas :) Fuuuuuui!').	
+	
---
+++ database2.pl
@@ -0,0 +1,81 @@
+% data_base
+
+definida_por('ethernet',
+	'Ethernet e uma arquitetura de interconexao para redes locais - Rede de Area Local - 
+baseada no envio de pacotes. Ela define cabeamento e sinais eletricos para a camada fisica, 
+em formato de pacotes e protocolos para a subcamada de controle de acesso ao meio do modelo OSI.').
+
+definida_por('endereçamento ethernet e seus serviços',
+	'Em um nivel de abstração mais alto, pode-se dizer que a tecnologia ethernet oferece a 
+comunicacao entre equipamentos de uma mesma rede fisica sem o uso de conexões e com servicos:
+
+• Unicast, ou seja, um quadro ethernet vai para um destino unico. Analogamente, pode-se tomar como
+exemplo o envio de um e-mail para uma unica pessoa; 
+
+• Broadcast, em que um quadro vai para todos os destinos, ou seja, o quadro e enviado para todas
+as maquinas de uma mesma rede;
+
+• Multicast, em que um quadro vai para multiplos destinos. Analogamente, pode-se tomar como
+exemplo o envio de um e-mail para um grupo de pessoas;').
+
+definida_por('protocolo MAC',
+	'Um endereco de controle de acesso a midia de um dispositivo e um identificador unico atribuido a 
+uma interface de rede. Para comunicacoes dentro de um segmento de rede, e usado como endereco de rede 
+para a maioria das tecnologias de rede IEEE 802, incluindo Ethernet, Wi-Fi e Bluetooth.').
+
+definida_por('gigabit ethernet',
+	'O padrao Gigabit Ethernet (ou 10/100/1000) foi criado em 1999 e continua sendo o tipo de rede mais recente. 
+Seu nome vem exatamente da velocidade prometida, de 1 Gbps, que e 10 vezes maior aquela oferecida pelo Fast Ethernet.').
+	 
+definida_por('metro ethernet',
+	'Uma rede de area metropolitana Ethernet, Ethernet MAN ou rede metro Ethernet e uma rede de 
+area metropolitana que se baseia em padroes Ethernet. E comumente usado para conectar assinantes a uma rede de servico 
+maior ou a Internet.').
+	 
+definida_por('protocolo TCP/IP',
+	'O TCP/IP e um conjunto de protocolos de comunicacao entre computadores em rede. Seu nome vem de dois protocolos: o TCP e o IP.').
+	 
+definida_por('protocolos que constituem o TCP',
+	'Aplicação, apresentação, sessão, transporte, redes, enlace e física.').
+	 
+definida_por('IGRP',
+	'Protocolo Interno de Distribuicao e um protocolo de distribuicao de dados do gateway dentro de uma complexa rede de computadores
+com varias rotas paralelas, desenvolvido na decada de 1980 pela Cisco Systems.').
+	 
+definida_por('EIGRP',
+	'O EIGRP e um protocolo avancado de roteamento por vetor da distancia, desenvolvido primeiramente como um protocolo proprietario da Cisco.').
+	 
+definida_por('SMTP',
+	'Protocolo de Transferência de Correio Simples é o protocolo padrão de envio de mensagens de correio eletronico atraves da Internet entre 
+dois dispositivos computacionais, definido na RFC 821.').
+	 
+definida_por('Telnet',
+	'Telnet e um protocolo de rede na Internet ou redes locais para proporcionar uma facilidade de 
+comunicacao baseada em texto interativo bidirecional usando uma conexao de terminal virtual.').
+
+definida_por('FTP',
+	'Protocolo de Transferencia de Arquivos e um protocolo padrao/generico independente de hardware 
+sobre um modo de transferir arquivos/ficheiros e tambem e um programa de transferencia. ').
+
+definida_por('http',
+	'O Hypertext Transfer Protocol, sigla HTTP e um protocolo de comunicacao utilizado para sistemas de 
+informacao de hipermidia, distribuidos e colaborativos. Ele e a base para a comunicacao de dados da World Wide Web.').
+
+definida_por('camada de rede',
+	'Fornecer os servicos de transferencia de dados fim a fim sobre uma rede, 
+independentemente das caracteristicas das subredes fisicas. Esta mais relacionada a topologia de
+rede e tem como uma de suas principais funcoes resolver problemas de roteamento em rede.').
+
+definida_por('IPv4',
+	'O Protocolo de Internet versao 4 e a quarta versao do Protocolo de Internet. 
+Ele e um dos principais protocolos de padroes baseados em metodos de interconexao de redes na Internet, 
+e foi a primeira versao implementada para a producao da ARPANET.').
+
+definida_por('ICMP',
+	'ICMP, sigla para o inglês Internet Control Message Protocol, é um protocolo integrante do Protocolo IP, 
+definido pelo RFC 792, é utilizado para comunicar informações da camada de rede, sendo o uso mais comum para 
+fornecer relatórios de erros à fonte original.').
+
+definida_por('exit',
+	'Te espero novamente para solucionar suas duvidas :) Fuuuuuui!').	
+	
